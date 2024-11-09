fccf62923e4afd69e143e0a0aab12a478d7da690
Dydy003 <dcaetano8@icloud.com>
2024-11-09 23:35:37 +0100

    Initial Commit

---
+++ FoodUb/FoodUbApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodUbApp.swift
+//  FoodUb
+//
+//  Created by Dylan Caetano on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodUbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
